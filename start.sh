78cc0ae8029f52fb2f4b962a1f7722979a1e4f27
GesundesWasser <83233602+GesundesWasser@users.noreply.github.com>
2024-03-16 13:04:28 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 mkdir cache
 curl -o cache/startserver.sh https://raw.githubusercontent.com/WWAGO-Inc/scripts/main/startserver.sh
-curl -o cache/load.sh https://raw.githubusercontent.com/WWAGO-Inc/scripts/main/updater.sh
+curl -o cache/load.sh https://raw.githubusercontent.com/WWAGO-Inc/scripts/main/load.sh
 chmod +x cache/startserver.sh
 chmod +x cache/load.sh
 ./cache/load.sh
