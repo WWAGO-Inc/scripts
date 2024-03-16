ff6fe85663b3a7f87a94fe747108696d8f15e112
GesundesWasser <83233602+GesundesWasser@users.noreply.github.com>
2024-03-16 12:40:40 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir cache
+curl -o cache/startserver.sh https://raw.githubusercontent.com/WWAGO-Inc/scripts/main/startserver.sh
+curl -o cache/load.sh https://raw.githubusercontent.com/WWAGO-Inc/scripts/main/updater.sh
+chmod +x cache/startserver.sh
+chmod +x cache/load.sh
+./cache/load.sh
